244de3c8e11a8370ba4f68fbf4617e811b568232
Richard Varela <richardvare13@gmail.com>
2026-05-03 12:31:25 -0700

    Created delete_note.py with the function delete_note(). Updated main.py to use the new function.

---
+++ src/delete_note.py
@@ -0,0 +1,15 @@
+def delete_note(line_number: int) -> None:
+    """
+    Deletes note from notes.txt file.
+    line_number: Integer parameter that has the line number of the note to delete.
+    """
+    line_number_count = 1
+    note_lines = None
+    with open("../notes.txt", 'r', encoding="utf-8") as notes_file:
+        note_lines = notes_file.readlines()
+        print(note_lines)
+    with open("../notes2.txt", 'w', encoding="utf-8") as notes_file:
+        for note_line in note_lines:
+            if line_number_count != line_number:
+                notes_file.write(note_line)
+            line_number_count += 1
