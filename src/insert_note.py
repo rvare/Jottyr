09157516dc4979074ddb52ab522e2bc8471b6292
Richard Varela <richardvare13@gmail.com>
2026-05-02 21:46:18 -0700

    Note insertion now handles if note is important or not. Also fixed error with the date_format parameter with insert_note(). Also updated main.py to reflect changes made.

--- src/insert_note.py
+++ src/insert_note.py
@@ -1,17 +1,22 @@
 import datetime
 import os
 
-def insert_note(note_content: str, date_format: str) -> None:
+def insert_note(note_content: str, date_format: str, important: bool) -> None:
     """
     Insert new note content to the notes.txt file.
     note_content: String that contains the users note content.
     date_format: Used to set what datetime format to use. Options:
         'datetime' for date and time with hours and minutes.
+    important: Boolean that indicates if the note has importance to it.
     """
     datetime_str_format = "%Y-%m-%d"
+    signifier = ""
     if date_format == "datetime":
         datetime_str_format = "%Y-%m-%dT%H:%M"
 
+    if important == True:
+        signifier = "(*) "
+
     iso_date = datetime.datetime.now().strftime(datetime_str_format)
     with open("../notes.txt", 'a', encoding="utf-8") as notes_file:
-        notes_file.write(f"{iso_date}  {note_content}\n")
+        notes_file.write(f"{signifier}{iso_date}  {note_content}\n")
--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import insert_note
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A notes.txt utility")
     parser.add_argument("-f", "--format", type=str, nargs=1, help="Chang formatting for input.")
-    parser.add_argument("-i", "--insert", type=str, nargs=1, help="Create a new note and append it to the notes.txt file.")
+    parser.add_argument("-n", "--new-note", type=str, nargs=1, help="Create a new note and append it to the notes.txt file.")
+    parser.add_argument("-i", "--important", type=bool, action=argparse.BooleanOptionalAction, help="Indicates if new note is important or note.")
     parser.add_argument("-d", "--delete", type=str, nargs=1, help="Delete a note.")
     # parser.add_argument("-p", "--print-format", type=str, nargs='*', help="Print notes. Options: all, range [date1, date2], last.")
     # parser.add_argument("-s", "--search", type=str, nargs='*', help="Search for an entry.")
@@ -13,8 +14,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f"args: {args}")
 
-    if args.insert:
+    if args.new_note:
         print("Insert")
-        insert_note.insert_note(args.insert[0], args.format)
+        insert_note.insert_note(args.new_note[0], args.format[0], args.important)
     elif args.delete:
         print("Delete")
