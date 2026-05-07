47c37879ff33b858d2c0a5855d68543879ee3831
Richard Varela <richardvare13@gmail.com>
2026-05-07 13:13:31 -0700

    In archival.py, changed how the function uses constants and instead of a wildcard import, it imports using the name. In constants.py, added ISO_DATE_FILE_FORMAT that's specifically for the archival file name.

--- src/archival.py
+++ src/archival.py
@@ -1,7 +1,7 @@
 import os
 import datetime
+import constants
 from pathlib import Path
-from  constants import *
 
 def archive() -> None:
     """
@@ -9,9 +9,11 @@ def archive() -> None:
     Will create a new file with %Y%m%d prefixed to it.
     """
     notes_list = None
-    with open(f"{Path.home()}/{NOTES_PATH}", 'r', encoding="utf-8") as notes_file:
+    with open(f"{Path.home()}/{constants.NOTES_PATH}",
+                'r', encoding="utf-8") as notes_file:
         notes_list = notes_file.readlines()
 
-    iso_date = datetime.datetime.now().strftime("%Y%m%d")
-    with open(f"{Path.home()}/archive_{iso_date}.txt", 'w', encoding="utf-8") as archive_file:
+    iso_date = datetime.datetime.now().strftime(constants.ISO_DATE_FILE_FORMAT)
+    with open(f"{Path.home()}/archive_{iso_date}.txt",
+                'w', encoding="utf-8") as archive_file:
         archive_file.writelines(notes_list);
--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 """Contains constants for other functions to use."""
 NOTES_PATH = "notes.txt"
+NOTE_SIGNIFIER = "(*) "
 ISO_DATE_FORMAT = "%Y-%m-%d"
 ISO_DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
-NOTE_SIGNIFIER = "(*) "
+ISO_DATE_FILE_FORMAT = "%Y%m%d"
 # ARCHIVE_PATH # TODO Later, will try to find a way to use directories.
