905deb404bc2bea2120bd127f300e8c6d270421a
Richard Varela <richardvare13@gmail.com>
2026-05-17 12:35:53 -0700

    Added exception handling in main.py.

--- src/main.py
+++ src/main.py
@@ -33,26 +33,31 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.new_note:
-        new_note(args.format[0] if args.format else None, args.important)
-    elif args.search:
-        try:
+    try:
+        if args.new_note:
+            new_note(args.format[0] if args.format else None, args.important)
+        elif args.search:
             found_notes = regex_search(args.search[0])
             for note in found_notes:
                 print(note)
-        except re.error:
-            print(f"ERROR: The regex {args.search[0]} is not valid")
-            print(f"Common Error: you're escaping sequences, "
-                    + "you need to use double backslashes.")
-            print("Else, review Python regex syntax.")
-        except Exception as exception:
-            print(exception)
-    elif args.delete:
-        delete_note(args.delete[0])
-    elif args.archive:
-        archive()
-    elif args.output:
-        html_output()
-    else:
-        print("ERROR: Must give parameters. Do --help to see all parameters.")
-
+        elif args.delete:
+            delete_note(args.delete[0])
+        elif args.archive:
+            archive()
+        elif args.output:
+            html_output()
+        else:
+            print("ERROR: Must give parameters. Do --help to see all parameters.")
+    except KeyboardInterrupt as ki_ex: # For now, we'll exit when a control signal happens.
+        exit()
+    except re.error:
+        print(f"ERROR: The regex {args.search[0]} is not valid")
+        print(f"Common Error: you're escaping sequences, "
+              + "you need to use double backslashes.")
+        print("Else, review Python regex syntax.")
+    except FileNotFoundError as file_not_found_er:
+        print(file_not_found_er)
+    except PermissionError as permission_er:
+        print(permission_er)
+    except Exception as exception:
+        print(exception)
